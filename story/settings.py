67e88a51211f1ed026cfe850f24e78f75f4267cc
shinelikeasmile <shinelikeasmile@gmail.com>
2020-06-27 12:28:05 +0530

    Intital Commit

--- story/settings.py
+++ story/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '+rxz52inxzfrug-8b$b63g5m**ba@6*7&h1%##ce!%08$+d4-)'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
